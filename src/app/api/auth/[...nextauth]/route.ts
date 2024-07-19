cb1bcc8b8cffee26de07b824e01272dfd57649cd
miftt <miftahfauzi012@gmail.com>
2024-07-19 18:50:37 +0700

    rollback

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,86 +1,9 @@
-import nextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { login } from "@/lib/prisma/service";
-import { compare } from "bcrypt";
+import { authOptions } from "@/lib/authOptions/authOptions";
+import NextAuth from "next-auth/next";
 
-const authOptions: NextAuthOptions = {
-    session: {
-        strategy: 'jwt',
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    providers: [
-        CredentialsProvider({
-            type: 'credentials',
-            name: 'Credentials',
-            credentials: {
-                username: {
-                    label: 'username',
-                    type: 'text',
-                    placeholder: 'username'
-                },
-                password: {
-                    label: 'Password',
-                    type: 'password',
-                    placeholder: 'Password'
-                },
-            },
-            async authorize(credentials) {
-                const { username, password } = credentials as {
-                    username: string;
-                    password: string;
-                };
-                // const user: any = {
-                //         id: 1,
-                //         name: "admin",
-                //         username: "admin",
-                //         role: "admin"
-                // }
-                // if(username === "admin" && password === "admin"){
-                //     return user;
-                // }else{
-                //     return null;
-                // }
-                const user:any = await login({username})
-                if (user){
-                    const passwordConfirm = await compare(password, user.password);
-                    if(passwordConfirm){
-                        return user;
-                    }
-                    return null;
-                }
-                else {
-                    return null;
-                };
-            }
-        })
-    ],
-    callbacks: {
-        async jwt({ token, account, profile, user, username}: any){
-            if (account?.provider === "credentials"){
-                token.role = user.role;
-                token.username = user.username;
-                token.id = user.id
-            }
-            return token
-        },
-        async session({ session, token }: any) {
-            if ("role" in token) {
-                session.user.role = token.role;
-            }
-            if ("username" in token) {
-                session.user.username = token.username;
-            }
-            if ("id" in token) {
-                session.user.id = token.id;
-            }
-            return session
-        }
-    },
-    pages: {
-        signIn: '/login',
-    }
+const handler = NextAuth(authOptions);
+
+export {
+    handler as GET,
+    handler as POST
 }
-
-const handler = nextAuth(authOptions);
-
-export { handler as GET, handler as POST }
