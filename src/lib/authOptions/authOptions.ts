0970ede4cbba46670bbabfa9b58c07440b362c8f
miftt <miftahfauzi012@gmail.com>
2024-07-19 17:57:40 +0700

    fix issue

--- src/lib/authOptions/authOptions.ts
+++ src/lib/authOptions/authOptions.ts
@@ -7,7 +7,7 @@ export const authOptions: NextAuthOptions = {
     session: {
         strategy: 'jwt',
     },
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET as string,
     providers: [
         CredentialsProvider({
             type: 'credentials',
