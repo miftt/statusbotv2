2545a2a55a8e15beddb271d2bf9ebe6e198472c1
miftt <miftahfauzi012@gmail.com>
2024-07-24 00:54:15 +0700

    fix getuser & add get status bot from public API

--- src/lib/prisma/service.ts
+++ src/lib/prisma/service.ts
@@ -79,7 +79,21 @@ export async function changePassword(userId: string, newPassword: string, oldPas
 }
 
 export async function getAllUser(){
-    const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            username: true,
+            role: true,
+            status: true,
+            token:{
+                select:{
+                    token: true
+                }
+            },
+            created_at: true,
+            expireDate: true
+        }
+    });
     return {
         status: true,
         statusCode: 200,
@@ -107,4 +121,32 @@ export async function getToken(userId: string){
     }else{
         return null;
     }
+}
+
+export async function getStatusBotPublicAPI(userId: string, token: string){
+    const user = await prisma.user.findUnique({
+        select:{
+            id: true,
+            status_bot: true,
+            token: {
+                select: {
+                    token: true
+                }
+            }
+        },
+        where:{
+            id: userId,
+            token: {
+                token: token
+            }
+        }
+    })
+    if(token !== user?.token?.token){
+        return 'Wrong Token!';
+    }else if(user){
+        return user?.status_bot;
+    }else {
+        return 'User not found!';
+    }
+
 }
