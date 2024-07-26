45b7c0a47b2582cf45fa41f79ff1041db3853066
miftt <miftahfauzi012@gmail.com>
2024-07-26 23:27:09 +0700

    add: api for delete user

---
+++ src/app/api/user/deleteuser/[id]/route.ts
@@ -0,0 +1,43 @@
+import { authOptions } from "@/lib/authOptions/authOptions"
+import { deleteUser } from "@/lib/prisma/service";
+import { getServerSession } from "next-auth"
+import { NextResponse } from "next/server";
+
+export const DELETE = async (request: Request, {params} : {params: {id: string}}) => {
+    const session = await getServerSession(authOptions);
+    if(!session){
+        return NextResponse.json({
+            status: false,
+            statusCode: 401,
+            error: "Unauthorized"
+        }, {
+            status: 401
+        })
+    }else if(session?.user?.role !== "admin"){
+        return NextResponse.json({
+            status: false,
+            statusCode: 401,
+            error: "Unauthorized"
+        }, {
+            status: 401
+        })
+    }else{
+        const res = await deleteUser(params.id);
+        if(res.status === false){
+            return NextResponse.json({
+                status: false,
+                statusCode: 400,
+                message: res.message
+            }, {
+                status: 400
+            })
+        }else if(res.status === true){
+            return NextResponse.json({
+                status: res.status,
+                statusCode: 200,
+                message: res.message,
+                username: res.data?.username
+            })
+        }
+    }
+}
